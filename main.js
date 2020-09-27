2cb0a09839598c156f1a47e7c16c28502e79dac9
yousef omar <yousefomar724@gmail.com>
2020-09-27 18:13:37 +0200

    added iframe files and some modifications

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menuItems = document.querySelector(".header .menu-items");
+const menuItem = document.querySelectorAll(".menu-item");
+const section = document.querySelectorAll(".section");
 
-menuBtn.addEventListener("click", () => {
+menuBtn.addEventListener("click", () => toggle())
+
+/* toggle of item click if open */
+menuItem.forEach((item) => {
+    item.addEventListener("click", () => {
+        if(menuBtn.classList.contains("open")) {
+            toggle();
+        };
+    });
+});
+
+section.forEach((item) => {
+    item.addEventListener("click", () => {
+        if(menuBtn.classList.contains("open")) {
+            toggle();
+        };
+    });
+});
+
+
+function toggle() {
     menuBtn.classList.toggle("open");
     menuItems.classList.toggle("open");
-})
+};
